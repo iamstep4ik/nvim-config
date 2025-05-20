5c45e1ef53b83101a64f4582015765924c90d4ae
Stepan Kovalev <lilop720@gmail.com>
2025-05-20 21:38:19 +0300

    new copilot

--- lua/stepan/plugins/github-copilot.lua
+++ lua/stepan/plugins/github-copilot.lua
@@ -5,7 +5,13 @@ return {
   event = "InsertEnter",
   config = function()
     require("copilot").setup({
-      suggestion = { enabled = true },
+      suggestion = {
+        enabled = true,
+        auto_trigger = true,
+        keymap = {
+          accept = "<Tab>",
+        },
+      },
       panel = { enabled = true },
     })
   end,
